135bfea11785854ea9ea28c14f7e8dbd39a9995d
nickgenov <nick.genov@hotmail.com>
2016-06-17 21:19:59 +0300

    Minor change.

---
+++ 02.StaticMembersAndNamespaces/HTMLDispatcher/ElementBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HTMLDispatcher
+{
+    public class ElementBuilder
+    {
+        private string elementName;
+        private string element;
+
+        public ElementBuilder(string elementName)
+        {
+            this.ElementName = elementName;
+        }
+
+        public string ElementName
+        {
+            get
+            {
+                return this.elementName;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("ElementName", "ElementName cannot be null or an empty string.");
+                }
+
+                this.elementName = value;
+            }
+        }
+
+        public void AddAttribute(string attribute, string value)
+        {
+            
+        }
+    }
+}
